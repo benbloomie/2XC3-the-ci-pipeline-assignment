b77951048001937e09c76cd8257d6eb6513d3c51
benbloomie <bloomfib@mcmaster.ca>
2024-10-01 19:00:30 -0400

    made changes, and added comments to script

--- addScript.sh
+++ addScript.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+# this script automates git add, commit, push and tag for quiz.c
+# Benjamin Bloomfield, McMaster University, 2024
 
 git add quiz.c
-git commit -m "$1"
-git tag "$2"
 
-git push origin main
-git push origin "$2"
+# checks if at least one argument (commit message) is provided
+if [[ $# -ge 1 ]]
+then
+	# commit using the argument as the message, and push the commit
+	git commit -m "$1"
+	git push origin main
+
+	# checks to see if two arguments (commit message and tag) are provided
+	if [[ $# -eq 2 ]]
+	then 
+		# tag, and push the tag
+		git tag $2
+		git push origin $2
+	fi
+else
+	echo "After calling the script, please enter <commit message (with quotes)> <tag name>"
